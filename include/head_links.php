a7e2fdb174df3ace65d85d106208d18d4c7033b5
Bashar Tukur Shehu <bashartukurshehu@gmail.com>
2025-07-28 13:30:33 +0100

    Add files via upload

---
+++ include/head_links.php
@@ -0,0 +1,12 @@
+<!-- mobile responsive meta -->
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+
+<!-- Link to Icons (font awesome icons) -->
+<link href="css/font-awesome.min.css" rel="stylesheet" type="text/css" media="all">
+
+<!-- Link to three different CSS stylesheet -->
+<link rel="stylesheet" type="text/css" href="css/styles.css">
+<link href="css/style-body.css" rel="stylesheet" type="text/css" media="all"/>
+<link href="css/style1.css" rel="stylesheet" type="text/css" media="all"/>
+<link rel="stylesheet" href="css/style-footer.css">
